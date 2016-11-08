93eb74098a6d5868402147695f434961a9e888ad
jvlara <jvlara>
2016-11-08 17:49:09 +0100

    http requests using mock services

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,14 @@
 import { NgModule }             from '@angular/core';
 import { BrowserModule }        from '@angular/platform-browser';
 import { FormsModule }          from '@angular/forms';
-import { RouterModule }         from '@angular/router';
+import { HttpModule }           from '@angular/http';
+import { HeroSearchComponent } from './hero-search.component';
+import './rxjs-extensions';
+
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent }         from './app.component';
 import { HeroesComponent }      from './heroes.component';
@@ -11,8 +18,8 @@ import { DashboardComponent }   from './dashboard.component';
 import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
-  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent ],
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule, HttpModule, InMemoryWebApiModule.forRoot(InMemoryDataService),],
+  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent, HeroSearchComponent ],
   providers: [HeroService],
   bootstrap:    [ AppComponent ]
 })
--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -23,6 +23,15 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
 
+  add(name: string): void {
+    name = name.trim();
+    if(!name) { return; }
+    this.heroService.create(name).then(hero => {
+      this.heroes.push(hero);
+      this.selectedHero = null;
+    })
+  }
+
   ngOnInit(): void {
     this.getHeroes();
   }
@@ -34,6 +43,17 @@ export class HeroesComponent implements OnInit {
   gotoDetail(): void {
     this.router.navigate(['/detail', this.selectedHero.id]);
   }
+
+  delete(hero: Hero): void{
+    console.log("delete hero");
+    this.heroService
+      .delete(hero.id)
+      .then(() =>{
+        this.heroes = this.heroes.filter(h => h ! == hero );
+        if(this.selectedHero === hero) { this.selectedHero = null; }
+      })
+  }
+
 }
 
 
